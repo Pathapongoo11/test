63c1ea85dd812edfb06fc20db31d71f8b56bdaed
Pathapongoo11 <31352484+Pathapongoo11@users.noreply.github.com>
2018-08-30 14:40:23 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -82,15 +82,7 @@ if (!is_null($events['events'])) {
 				
 			];
 
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/push';
-			$data = [
-			
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			
-			
+		
 			
 			
 			
