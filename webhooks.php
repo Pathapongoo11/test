28c382f3187a2f448fcce31d9d7ce49c3a72354c
Pathapongoo11 <31352484+Pathapongoo11@users.noreply.github.com>
2018-08-31 10:26:07 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -61,21 +61,24 @@ if (!is_null($events['events'])) {
 	
 			$messages1 = [
 				
-				 "type"=> "bubble",
- 				
-				    "type"=> "box",
-				    "layout"=> "horizontal",
-				    
-				    "type"=> "text",
+				
  			            "text"=> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod\n tempor incididunt ut labore et dolore magna aliqua.",
-					"wrap"=> true
+					
 			];
 
 				$url1 = 'https://api.line.me/v2/bot/message/multicast';
 							$data1 = [
 								'to' => "U052004ee1910a75874bcca55828e981f",
+								 "type"=> "bubble",
+								"body",
 								
+ 				
+								    "type"=> "box",
+								    "layout"=> "horizontal",
+								    "contents",
+								    "type"=> "text",
 								'message' => [$messages1]
+								"wrap"=> true
 								
 							];
 		
