78be9f21058308b1953f2f550dd2157a1e18f350
Pathapongoo11 <31352484+Pathapongoo11@users.noreply.github.com>
2018-09-02 09:26:42 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -60,7 +60,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 	
 			$messages1 = array (
-					  'to' => '$replyToken',
+					  'to' => $replyToken,
 					  'messages' => 
 					  array (
 					    0 => 
