0eff6666b21a12ca6e5a7d2925cb62c57a46f205
Pathapongoo11 <31352484+Pathapongoo11@users.noreply.github.com>
2018-09-02 10:05:38 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,10 +1,7 @@
 <?php // callback.php
-
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
-
 $access_token = 'XXAXYGNcIogeHDYMP+fSZ3/j3L/IWi5BgMFHk1FFmOXROxRETkstzwcv9DTH4QwaEGTznnIj6498MIj7TiZzC2yPEW/Rg4D03t//SWkjVr7Clnwz+iqZVP2NLAYsEo2xg6P//TgCgvbLab250OqmqFGUYhWQfeY8sLGRXgo3xvw=';
-
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -19,18 +16,16 @@ if (!is_null($events['events'])) {
 			$text = $event['source']['userId'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' =>  $text
+				'text' => $text
 			];
-
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => $messages,
+				'messages' => [$messages],
 			];
 			
 			
@@ -39,7 +34,6 @@ if (!is_null($events['events'])) {
 			
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -48,17 +42,17 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
 			curl_close($ch);
-
 			echo $result . "\r\n";
-		}else{
+		}
+	}
+}else{
 		$text = $event['source']['userId'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-
 			// Build message to reply back
 	
 			$messages1 = array (
-					  'to' => $text,
+					  'to' => 'U052004ee1910a75874bcca55828e981f',
 					  'messages' => 
 					  array (
 					    0 => 
@@ -92,7 +86,6 @@ if (!is_null($events['events'])) {
 					);
   
 		
-
 				$url1 = 'https://api.line.me/v2/bot/message/push';
 							$data1 = $messages1;
 		
@@ -109,10 +102,7 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch1, CURLOPT_FOLLOWLOCATION, 1);
 			$result1 = curl_exec($ch1);
 			curl_close($ch1);
-
 			echo $result1 . "\r\n";
 			
-            }
-	}
 }
 echo "OK";
